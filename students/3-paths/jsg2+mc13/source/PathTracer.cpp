e090b79d32c4dce1ddf02d3940a1f3c37f7da12d
jsg2 <jsg2@61224a88-a156-e611-bf22-b8ac6f268050>
2016-10-01 20:53:06 +0000

    Wrote writeToImage

--- students/3-paths/jsg2+mc13/source/PathTracer.cpp
+++ students/3-paths/jsg2+mc13/source/PathTracer.cpp
@@ -37,7 +37,7 @@ void PathTracer::traceImage(const shared_ptr<Camera>& cam, const shared_ptr<Imag
             for(int j(0); j < numPixels; ++j){computeShadowRays(shadowRayBuffer, surfelBuffer, biradianceBuffer, lights, j); }    
             for(int j(0); j < numPixels; ++j){testVisibility(shadowRayBuffer, lightShadowedBuffer, j); }    //casts shadow rays
             // Increments image pixels -> by emissive and direct light, with importance sampling
-            for(int j(0); j < numPixels; ++j){writeToImage(shadowRayBuffer, biradianceBuffer, surfelBuffer, modulationBuffer, j);}
+            for(int j(0); j < numPixels; ++j){writeToImage(shadowRayBuffer, biradianceBuffer, surfelBuffer, modulationBuffer, j, image, rayBuffer, lights);}
             //Only if not last iteration.
             for(int j(0); j < numPixels; ++j){generateRecursiveRays(rayBuffer, surfelBuffer, j);}    //set up for next loop
            // for(int j(0); j < numPixels; ++j){updateModulation(modulationBuffer, rayBuffer, j);}    
@@ -47,25 +47,33 @@ void PathTracer::traceImage(const shared_ptr<Camera>& cam, const shared_ptr<Imag
 
 };
 
+void PathTracer::writeToImage(const Array<Ray>& shadowRayBuffer, const Array<Radiance3>& biradianceBuffer, const Array<shared_ptr<Surfel>>& surfelBuffer, const Array<Color3>& modulationBuffer, const int j, const shared_ptr<Image>& image, const Array<Ray>& rayBuffer, const Array<shared_ptr<Light>>& lights) const{
+    Color3 current;
+    image->get(Point2int32(j%image->width(), j/image->width()), current);
+    current += L_out(surfelBuffer[j], -rayBuffer[j].direction(), lights);
+    image->set(Point2int32(j%image->width(), j/image->width()), current);
+};
+
+//Don't use anymore
 Radiance3 PathTracer::L_in(const Point3& X, const Vector3& w_in, int pathDepth, const TriTree& triArray) const {
     // Find the first intersection 
     const shared_ptr<Surfel>& surfel(triArray.intersectRay(Ray(X, w_in)));
 
     // Compute the light leaving Y, which is the same as
         // the light entering X when the medium is non-absorptive
-    return L_out(surfel, -w_in, pathDepth, triArray);
+    return Radiance3();//L_out(surfel, -w_in, pathDepth, triArray);
 
 };
-Radiance3 PathTracer::L_out(const shared_ptr<Surfel>& surfel, const Vector3& w_out, int pathDepth, const TriTree& triArray) const {
+
+Radiance3 PathTracer::L_out(const shared_ptr<Surfel>& surfel, const Vector3& w_out, const Array<shared_ptr<Light>>& lights) const {
     if (notNull(surfel)) {
         const Radiance3& L(surfel->emittedRadiance); // Emitted
 
         const Vector3& n(surfel->shadingNormal);
-        const Vector3& w_in(Vector3::cosHemiRandom(n, Random::threadCommon()));
-        const Point3& X(surfel->position*EPSILON* sign((w_in).dot(n)));
+        //const Vector3& w_in(Vector3::cosHemiRandom(n, Random::threadCommon()));
+        const Point3& X(surfel->position*.0001* sign((w_out).dot(n)));
 
-
-        Array<shared_ptr<Light>> lights(m_scene->lightingEnvironment().lightArray);
+        //Will be replaced with importance sampling
         for (int i = 0; i < lights.size(); ++i) {
             const shared_ptr<Light>& light(lights[i]);
             const Point3& Y = light->position().xyz();
@@ -79,14 +87,14 @@ Radiance3 PathTracer::L_out(const shared_ptr<Surfel>& surfel, const Vector3& w_o
             }
         }
 
-        Radiance3& indirectLight(Radiance3(0, 0, 0));
+        /*Radiance3& indirectLight(Radiance3(0, 0, 0));
         if (m_maxScatters < pathDepth) {
             const Vector3& w_i(Vector3::cosHemiRandom(n, Random::threadCommon()));
             const Color3& f(surfel->finiteScatteringDensity(w_i, -w_out));
             indirectLight += L_in(X, w_i, pathDepth + 1, triArray)*f;
-        }
+        }*/
 
-        return L + indirectLight / m_maxScatters;
+        return L;
 
     }
     else {
